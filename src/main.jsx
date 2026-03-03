d117f7bd96723e361de87d2f19bc5d9522d46e34
Rahul Prasad <rahul.prasad@itmtb.com>
2026-03-04 01:32:04 +0530

    Update UI and booking logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from "./App"
 import { AuthProvider } from "./context/AuthContext"
 import { BookingProvider } from "./context/BookingContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <AuthProvider>
       <BookingProvider>
         <App />
       </BookingProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
